0c8a594d2a6ac566bcc9e6eaa0516ce4262843ac
Carlos "Kurolox" Gomez <contact@carlosg.dev>
2019-10-02 16:52:46 +0200

    Finished check_existing_event function

--- src/orquest_to_gcal.py
+++ src/orquest_to_gcal.py
@@ -1,10 +1,16 @@
+import calendar
 import configparser
 import datetime
-import calendar
+import pickle
 from os import path
+
 import requests
+from google.auth.transport.requests import Request
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
 
 SETTINGS_FILE_NAME = "config.ini"
+SCOPES = ["https://www.googleapis.com/auth/calendar"]
 
 
 def main():
@@ -25,7 +31,7 @@ def main():
             "The settings file is missing the [orquest] section. Aborting...")
         return
 
-    # TODO: Use Google Calendar API to add work shifts to calendar
+    add_shifts_to_calendar(work_json, **config["google_calendar"])
 
 
 def get_orquest_data(url, username, password):
@@ -44,6 +50,7 @@ def get_orquest_data(url, username, password):
 
     month_schedule = session.get(
         url + f"/rest/User/AssignmentsInfo/{calendar_range[0]}/{calendar_range[1]}")
+
     return month_schedule.json()
 
 
@@ -56,10 +63,71 @@ def calendar_month_range(date):
     first_monday += datetime.timedelta(days=-first_monday.weekday())
 
     # Get last day from current month, then get sunday of that week
-    last_sunday = date.replace(day=calendar.monthlen(date.year, date.month)).date()
+    last_sunday = date.replace(
+        day=calendar.monthlen(date.year, date.month)).date()
     last_sunday += datetime.timedelta(days=6 - date.weekday())
 
     return first_monday.isoformat(), last_sunday.isoformat()
+
+
+def add_shifts_to_calendar(work_json, json_path, calendar_id):
+    """Shows basic usage of the Google Calendar API.
+    Prints the start and name of the next 10 events on the user"s calendar.
+    """
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if path.exists("token.pickle"):
+        with open("token.pickle", "rb") as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                path.join(path.dirname(__file__), json_path), SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open("token.pickle", "wb") as token:
+            pickle.dump(creds, token)
+
+    service = build("calendar", "v3", credentials=creds)
+
+    # Call the Calendar API
+    now = datetime.datetime.utcnow().isoformat() + "Z"  # "Z" indicates UTC time
+    events_result = service.events().list(calendarId=calendar_id, timeMin=now,
+                                          maxResults=10, singleEvents=True,
+                                          orderBy="startTime").execute()
+    events = events_result.get("items", [])
+
+    # Shifts where I've been included as a worker
+    for shift in [day for day in work_json["assignments"] if day["presence"]["worked"]]:
+        if not check_existing_event(calendar_id, service, shift["day"]):
+            create_event(calendar_id, service, shift)
+
+
+def check_existing_event(calendar, service, day):
+    """Checks if there's already an event (or events) in any given day.
+    Returns a list with all of the event ID's."""
+
+    # Create a datetime object from the ISO 8601 string
+
+    start = datetime.datetime(*[int(field) for field in day.split("-")],
+                              0, 0, tzinfo=datetime.datetime.utcnow().astimezone().tzinfo)
+    end = start + datetime.timedelta(days=1) - datetime.timedelta(seconds=1)
+
+    query = service.events().list(calendarId=calendar, singleEvents=True,
+                                  timeMin=start.isoformat(), timeMax=end.isoformat()).execute()
+
+
+    return query.get("items", [])
+
+
+def create_event(calendar, service, day):
+    """Adds an event to the configured calendar."""
+    return None
 
 
 if __name__ == "__main__":
